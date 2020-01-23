35ce963200a1feb22a77c7100211a44a0bd8ec1f
Matt Clarke <matt.clarke@esss.se>
2020-01-23 13:16:54 +0100

    Better testing of kafka tools

---
+++ tests/test_kafka_tools.py
@@ -0,0 +1,16 @@
+from just_bin_it.endpoints.kafka_tools import are_brokers_present, are_topics_present
+from tests.mock_consumer import MockConsumer
+
+
+def test_checking_for_non_existent_broker_returns_none():
+    assert are_brokers_present(["not_present"]) is None
+
+
+def test_checking_for_non_existent_topic_returns_false():
+    consumer = MockConsumer(["broker"], ["topic1"])
+    assert not are_topics_present(consumer, ["not_present"])
+
+
+def test_checking_for_existing_topic_returns_false():
+    consumer = MockConsumer(["broker"], ["topic1"])
+    assert are_topics_present(consumer, ["topic1"])
