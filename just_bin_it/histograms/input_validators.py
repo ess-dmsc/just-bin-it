84404abf021d64e08b95984e851ae7fd28982439
Michele Brambilla <michele.brambilla@psi.ch>
2020-12-23 10:30:08 +0100

    Allow initialise 2Dhistogram with tuple for num_bins

--- just_bin_it/histograms/input_validators.py
+++ just_bin_it/histograms/input_validators.py
@@ -1,14 +1,37 @@
+from collections.abc import Iterable
+
 from just_bin_it.exceptions import JustBinItException
+
+
+def is_valid(
+    value,
+    dtype: type = None,
+    length: int = None,
+    limits: tuple = None,
+    validator: object = None,
+) -> bool:
+    if dtype:
+        if not isinstance(value, dtype):
+            return False
+        if isinstance(value, Iterable) and not all(
+            isinstance(x, int) and x >= 0 for x in value
+        ):
+            return False
+    if length and len(value) != length:
+        print(f'length and len(value): {length}, {len(value)}')
+        return False
+    if limits and not (limits[0] <= value <= limits[1]):
+        return False
+    if validator and not validator(value):
+        return False
+    return True
 
 
 def check_tof(tof_range, missing, invalid):
     if tof_range is None:
         missing.append("TOF range")  # pragma: no mutate
-    elif (
-        not isinstance(tof_range, list)
-        and not isinstance(tof_range, tuple)
-        or len(tof_range) != 2
-    ):
+        return
+    if not is_valid(tof_range, dtype=Iterable, length=2):
         invalid.append("TOF range")  # pragma: no mutate
 
 
@@ -17,19 +40,17 @@ def check_bins(num_bins, missing, invalid):
         missing.append("number of bins")  # pragma: no mutate
         return
 
-    if isinstance(num_bins, int):
-        if num_bins < 1:
-            invalid.append("Number of bins")
-            return
-        check_int(num_bins, "number of bins", invalid)
-    elif isinstance(num_bins, (list, tuple)):
-        if len(num_bins) != 2:
-            invalid.append("Dimension of number of bins")
-            return
-        check_int(num_bins[0], "x", invalid)
-        check_int(num_bins[1], "y", invalid)
-    else:
-        invalid.append("Unknown type of num_bins")
+    if not (
+        is_valid(num_bins, dtype=int, validator=lambda x: x > 0)
+        or is_valid(
+            num_bins,
+            dtype=Iterable,
+            length=2,
+            validator=lambda x: all(y > 0 for y in x),
+        )
+    ):
+        invalid.append("number of bins")  # pragma: no mutate
+        return
 
 
 def check_int(value, field, invalid):
@@ -43,11 +64,8 @@ def check_int(value, field, invalid):
 def check_det_range(det_range, missing, invalid):
     if det_range is None:
         missing.append("Detector range")  # pragma: no mutate
-    elif (
-        not isinstance(det_range, list)
-        and not isinstance(det_range, tuple)
-        or len(det_range) != 2
-    ):
+        return
+    if not is_valid(det_range, dtype=Iterable, length=2):
         invalid.append("Detector range")  # pragma: no mutate
 
 
@@ -55,12 +73,8 @@ def generate_exception(missing, invalid, hist_type):
     error_msg = ""
 
     if missing:
-        error_msg += (
-            f"Missing information for {hist_type} histogram: {', '.join(missing)}"
-        )  # pragma: no mutate
+        error_msg += f"Missing information for {hist_type} histogram: {', '.join(missing)}"  # pragma: no mutate
     if invalid:
-        error_msg += (
-            f"Invalid information for {hist_type} histogram:  {', '.join(missing)}"
-        )  # pragma: no mutate
+        error_msg += f"Invalid information for {hist_type} histogram:  {', '.join(missing)}"  # pragma: no mutate
     if error_msg:
         raise JustBinItException(error_msg)
--- tests/test_histogram2d.py
+++ tests/test_histogram2d.py
@@ -3,11 +3,110 @@ import pytest
 
 from just_bin_it.exceptions import JustBinItException
 from just_bin_it.histograms.histogram2d import Histogram2d
+from just_bin_it.histograms.input_validators import is_valid
 
 IRRELEVANT_TOPIC = "some-topic"
 IRRELEVANT_NUM_BINS = (123, 456)
 IRRELEVANT_TOF_RANGE = (0, 100)
 IRRELEVANT_DET_RANGE = (0, 100)
+
+
+def test_validator_returns_true_if_conditions_are_satisfied():
+    assert is_valid(1, dtype=int, limits=(-2, 2))
+    assert is_valid(1, dtype=int, validator=lambda x: x * x - 2 * x + 1 >= 0)
+
+    assert is_valid(1.45, dtype=float, limits=(-2, 2))
+    assert is_valid(1.45, dtype=float, validator=lambda x: x * x - 2 * x + 1 >= 0)
+
+    assert is_valid([1, 2], dtype=list, length=2)
+    assert is_valid(list(range(10)), dtype=list, length=10)
+    assert is_valid(list(range(10)), dtype=list, length=10, validator=lambda x: any(x))
+
+    assert is_valid((1, 2), dtype=tuple, length=2)
+    assert is_valid(tuple(range(10)), dtype=tuple, length=10)
+    assert is_valid(
+        tuple(range(10)), dtype=tuple, length=10, validator=lambda x: any(x)
+    )
+
+
+def test_validator_returns_false_if_a_condition_is_not_satisfied():
+    assert not is_valid(1, dtype=float)
+    assert not is_valid(1, limits=(2, 4))
+    assert not is_valid(1, validator=lambda x: x * x - 2 * x + 1 > 0)
+
+    assert not is_valid(1.45, dtype=int)
+    assert not is_valid(1.45, limits=(-4, -2))
+    assert not is_valid(1.45, validator=lambda x: x * x - 2 * x + 1 > 10)
+
+    assert not is_valid([1, 2], dtype=tuple)
+    assert not is_valid([1, 2], dtype=int)
+    assert not is_valid(list(range(10)), length=5)
+    assert not is_valid(list(range(10)), validator=lambda x: any([n < 0 for n in x]))
+
+    assert not is_valid((1, 2), dtype=list)
+    assert not is_valid((1, 2), dtype=int)
+    assert not is_valid(tuple(range(10)), length=5)
+    assert not is_valid(tuple(range(10)), validator=lambda x: any([n < 0 for n in x]))
+
+
+def test_validator_returns_false_if_any_condition_is_not_satisfied():
+    assert not is_valid(
+        1, dtype=float, limits=(-2, 2), validator=lambda x: x * x - 2 * x + 10 > 0
+    )
+    assert not is_valid(
+        1, dtype=int, limits=(-3, -2), validator=lambda x: x * x - 2 * x + 10 > 0
+    )
+    assert not is_valid(
+        1, dtype=int, limits=(-2, 2), validator=lambda x: x * x - 2 * x - 10 > 0
+    )
+
+    assert not is_valid(
+        1.45, dtype=int, limits=(-5, 5), validator=lambda x: x * x - 2 * x + 10 > 0
+    )
+    assert not is_valid(
+        1.45, dtype=float, limits=(-5, -2), validator=lambda x: x * x - 2 * x + 10 > 0
+    )
+    assert not is_valid(
+        1.45, dtype=float, limits=(-5, 5), validator=lambda x: x * x - 2 * x - 10 > 0
+    )
+
+    assert not is_valid(
+        list(range(10)),
+        dtype=tuple,
+        length=10,
+        validator=lambda x: any([n > 0 for n in x]),
+    )
+    assert not is_valid(
+        list(range(10)),
+        dtype=list,
+        length=1,
+        validator=lambda x: any([n > 0 for n in x]),
+    )
+    assert not is_valid(
+        list(range(10)),
+        dtype=list,
+        length=10,
+        validator=lambda x: any([n < 0 for n in x]),
+    )
+
+    assert not is_valid(
+        tuple(range(10)),
+        dtype=list,
+        length=10,
+        validator=lambda x: any([n > 0 for n in x]),
+    )
+    assert not is_valid(
+        tuple(range(10)),
+        dtype=tuple,
+        length=1,
+        validator=lambda x: any([n > 0 for n in x]),
+    )
+    assert not is_valid(
+        tuple(range(10)),
+        dtype=tuple,
+        length=10,
+        validator=lambda x: any([n < 0 for n in x]),
+    )
 
 
 class TestHistogram2dFunctionality:
@@ -119,6 +218,80 @@ class TestHistogram2dFunctionality:
         assert self.hist.last_pulse_time == 1236
 
 
+class TestHistogram2dBinsTupleFunctionality:
+    @pytest.fixture(autouse=True)
+    def prepare(self):
+        self.pulse_time = 1234
+        self.num_bins = (5, 10)
+        self.tof_range = (0, 10)
+        self.det_range = (0, 5)
+        if is_valid(self.num_bins, dtype=tuple, length=2):
+            self.data = np.array(
+                [
+                    x + y
+                    for x in range(self.num_bins[0])
+                    for y in range(self.num_bins[1])
+                ]
+            )
+        self.hist = Histogram2d("topic", self.num_bins, self.tof_range, self.det_range)
+
+    def test_on_construction_histogram_is_uninitialised(self):
+        assert self.hist.x_edges is not None
+        assert self.hist.y_edges is not None
+        assert self.hist.shape == self.num_bins
+        assert len(self.hist.x_edges) == self.num_bins[0] + 1
+        assert len(self.hist.y_edges) == self.num_bins[1] + 1
+
+        assert self.hist.x_edges[0] == self.data[0]
+        assert self.hist.x_edges[-1] == 10
+        assert self.hist.y_edges[0] == self.data[0]
+        assert self.hist.y_edges[-1] == 5
+        assert self.hist.data.sum() == 0
+
+    def test_adding_data_outside_initial_bins_is_ignored(self):
+        self.hist.add_data(self.pulse_time, self.data, self.data)
+        first_sum = self.hist.data.sum()
+        x_edges = self.hist.x_edges[:]
+        y_edges = self.hist.y_edges[:]
+
+        # Add data that is outside the edges
+        new_data = np.array([x + self.num_bins[0] + 1 for x in range(self.num_bins[0])])
+        new_data = np.append(
+            new_data, [x + self.num_bins[1] + 1 for x in range(self.num_bins[1])]
+        )
+        self.hist.add_data(self.pulse_time, new_data, new_data)
+
+        # Sum should not change
+        assert self.hist.data.sum() == first_sum
+        # Edges should not change
+        assert np.array_equal(self.hist.x_edges, x_edges)
+        assert np.array_equal(self.hist.y_edges, y_edges)
+
+    def test_only_data_with_correct_source_is_added(self):
+        hist = Histogram2d(
+            "topic", self.num_bins, self.tof_range, self.det_range, source="source1"
+        )
+
+        hist.add_data(self.pulse_time, self.data, self.data, source="source1")
+        hist_data_sum = hist.data.sum()
+
+        hist.add_data(self.pulse_time, self.data, self.data, source="source1")
+        hist.add_data(self.pulse_time, self.data, self.data, source="OTHER")
+
+        assert hist.data.sum() == 2 * hist_data_sum
+
+    def test_after_clearing_histogram_can_add_data(self):
+        self.hist.add_data(self.pulse_time, self.data, self.data)
+        hist_data_sum = self.hist.data.sum()
+
+        self.hist.clear_data()
+
+        self.hist.add_data(self.pulse_time, self.data, self.data)
+
+        assert self.hist.shape == self.num_bins
+        assert self.hist.data.sum() == hist_data_sum
+
+
 class TestHistogram2dConstruction:
     def test_if_tof_missing_then_histogram_not_created(self):
         with pytest.raises(JustBinItException):
@@ -199,3 +372,16 @@ class TestHistogram2dConstruction:
         )
 
         assert histogram.identifier == "123456"
+
+    def test_config_with_tuple_as_bins_parameter(self):
+        bins = (5, 10)
+        histogram = Histogram2d(
+            topic=IRRELEVANT_TOPIC,
+            num_bins=bins,
+            tof_range=IRRELEVANT_TOF_RANGE,
+            det_range=IRRELEVANT_DET_RANGE,
+        )
+
+        assert histogram.num_bins == bins
+        assert len(histogram.x_edges) == bins[0] + 1
+        assert len(histogram.y_edges) == bins[1] + 1
