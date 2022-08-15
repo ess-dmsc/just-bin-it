0808592989085ee85cac5f8815ba702e838fa86a
Matt Clarke <matt.clarke@esss.se>
2022-08-15 13:28:52 +0200

    improved tests for det maps.

--- tests/test_histogram2d_map.py
+++ tests/test_histogram2d_map.py
@@ -26,22 +26,12 @@ class TestHistogram2dMapFunctionality:
     @pytest.fixture(autouse=True)
     def prepare(self):
         self.pulse_time = 1234
-        self.det_range = (1, 30)
+        self.det_range = (1, 31)
         self.width = 5
         self.height = 6
         self.data = generate_image(self.width, self.height)
 
         self.hist = DetHistogram("topic", self.det_range, self.width, self.height)
-
-    def test_adding_data_to_detector_range_which_does_not_start_at_zero_is_okay(self):
-        hist = DetHistogram(IRRELEVANT_TOPIC, (3, 30), self.width, self.height)
-
-        hist.add_data(self.pulse_time, IRRELEVANT_TOF_RANGE, self.data)
-
-        # The data for det_ids 1 and 2 are discarded.
-        assert hist.data.sum() == len(self.data) - self.data.count(1) - self.data.count(
-            2
-        )
 
     def test_on_construction_histogram_is_uninitialised(self):
         assert self.hist.x_edges is not None
@@ -54,7 +44,27 @@ class TestHistogram2dMapFunctionality:
         assert self.hist.y_edges[-1] == 6
         assert self.hist.data.sum() == 0
 
-    def test_binning(self):
+    def test_data_before_detector_range_ignored(self):
+        hist = DetHistogram(IRRELEVANT_TOPIC, (3, 32), self.width, self.height)
+
+        hist.add_data(self.pulse_time, IRRELEVANT_TOF_RANGE, self.data)
+
+        # The data for det_ids 1 and 2 are discarded.
+        assert hist.data.sum() == len(self.data) - self.data.count(1) - self.data.count(
+            2
+        )
+        assert hist.data[3][5] == 0
+        assert hist.data[4][5] == 0
+
+    def test_data_after_detector_range_ignored(self):
+        hist = DetHistogram(IRRELEVANT_TOPIC, (0, 30), self.width, self.height)
+
+        hist.add_data(self.pulse_time, IRRELEVANT_TOF_RANGE, self.data)
+
+        # data for det id 30 should not be counted
+        assert hist.data.sum() == len(self.data) - self.data.count(30)
+
+    def test_check_binning(self):
         self.hist.add_data(self.pulse_time, [], self.data)
 
         shape = self.hist.shape
@@ -67,7 +77,7 @@ class TestHistogram2dMapFunctionality:
                 expected += 1
                 assert data[x][y] == expected
 
-    def test_adding_data_to_initialised_histogram_new_data_is_added(self):
+    def test_adding_data_accumulates_histogram(self):
         self.hist.add_data(self.pulse_time, [], self.data)
         first_sum = self.hist.data.sum()
 
@@ -152,13 +162,25 @@ class TestHistogram2dMapFunctionality:
         assert self.hist.data.sum() == len(self.data)
 
     def test_adding_empty_data_does_nothing(self):
-        self.hist.add_data(self.pulse_time, [], [])
+        self.hist.add_data(self.pulse_time, IRRELEVANT_TOF_RANGE, [])
 
         assert self.hist.data.sum() == 0
 
     def test_histogram_keeps_track_of_last_pulse_time_processed(self):
-        self.hist.add_data(1234, [], self.data)
-        self.hist.add_data(1235, [], self.data)
-        self.hist.add_data(1236, [], self.data)
+        self.hist.add_data(1234, IRRELEVANT_TOF_RANGE, self.data)
+        self.hist.add_data(1235, IRRELEVANT_TOF_RANGE, self.data)
+        self.hist.add_data(1236, IRRELEVANT_TOF_RANGE, self.data)
 
         assert self.hist.last_pulse_time == 1236
+
+    def test_histograms_corners(self):
+        # Test reproduces a bug that was fixed
+        self.hist.add_data(1234, IRRELEVANT_TOF_RANGE, [1])
+        self.hist.add_data(1235, IRRELEVANT_TOF_RANGE, [5, 5])
+        self.hist.add_data(1236, IRRELEVANT_TOF_RANGE, [26, 26, 26])
+        self.hist.add_data(1236, IRRELEVANT_TOF_RANGE, [30, 30, 30, 30])
+
+        assert self.hist.data[0][0] == 1
+        assert self.hist.data[4][0] == 2
+        assert self.hist.data[0][5] == 3
+        assert self.hist.data[4][5] == 4
