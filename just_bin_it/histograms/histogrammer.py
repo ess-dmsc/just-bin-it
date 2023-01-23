55af6f255b2f348ed72e6bb4fa25859064aa56c2
Matt Clarke <matt.clarke@esss.se>
2023-01-23 11:29:06 +0100

    make it work with the new tuple

--- just_bin_it/histograms/histogrammer.py
+++ just_bin_it/histograms/histogrammer.py
@@ -49,8 +49,8 @@ class Histogrammer:
                         continue
 
                 self._started = True
-                src = msg.source_name if not simulation else hist.source
-                hist.add_data(msg.pulse_time, msg.time_of_flight, msg.detector_id, src)
+                src = msg[0] if not simulation else hist.source
+                hist.add_data(msg[1], msg[2], msg[3], src)
 
     def publish_histograms(self, timestamp=0):
         """
