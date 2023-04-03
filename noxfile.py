995371fcd8ac0f8720ccac14eb15f9fc1267d944
Matt Clarke <matt.clarke@ess.eu>
2023-04-03 10:56:29 +0200

    run flake8 via nox too

--- noxfile.py
+++ noxfile.py
@@ -3,9 +3,18 @@ import nox
 
 # An example nox task definition that runs on many supported Python versions:
 @nox.session(
-    python=["3.10", "3.9"]
+    python=["3.8", "3.9", "3.10"]
 )
-def test(session):
+def tests(session):
     session.install("-r", "requirements-dev.txt")
 
     session.run("pytest", *session.posargs)
+
+
+@nox.session(
+    python=["3.10", "3.9"]
+)
+def flake8(session):
+    session.install("-r", "requirements-dev.txt")
+
+    session.run("flake8", "tests", "just_bin_it")
