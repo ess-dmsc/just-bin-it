b2f59599d5de640e0f834ae0085778f8da72ea84
Afonso Mukai <afonso.mukai@ess.eu>
2023-05-08 11:09:13 +0200

    Add kafka_security tests

---
+++ tests/test_kafka_security.py
@@ -0,0 +1,44 @@
+import pytest
+
+from just_bin_it.endpoints.kafka_security import get_kafka_security_config
+
+
+class TestKafkaSecurity:
+    def test_unsupported_protocol_throws(self):
+        with pytest.raises(Exception):
+            get_kafka_security_config(
+                "SASL_SSL",
+            )
+
+    def test_unsupported_sasl_mechanism_throws(self):
+        with pytest.raises(Exception):
+            get_kafka_security_config(
+                "SASL_PLAINTEXT",
+                "UNSUPPORTED_PROTOCOL",
+                "username",
+                "password",
+            )
+
+    def test_missing_sasl_username_throws(self):
+        with pytest.raises(Exception):
+            get_kafka_security_config(
+                "SASL_PLAINTEXT",
+                "PLAIN",
+                None,
+                "password",
+            )
+
+    def test_missing_sasl_password_throws(self):
+        with pytest.raises(Exception):
+            get_kafka_security_config(
+                "SASL_PLAINTEXT",
+                "PLAIN",
+                "username",
+            )
+
+    def test_missing_sasl_usename_and_password_throws(self):
+        with pytest.raises(Exception):
+            get_kafka_security_config(
+                "SASL_PLAINTEXT",
+                "PLAIN",
+            )
