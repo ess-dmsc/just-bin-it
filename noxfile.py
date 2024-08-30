19ae4be003cd5fc21b05e6aee33ebc160bf449b0
Matt Clarke <matt.clarke@ess.eu>
2024-08-30 08:27:27 +0200

    re-add isort

--- noxfile.py
+++ noxfile.py
@@ -1,17 +1,13 @@
 import nox
 
 
-@nox.session(
-    python=["3.8", "3.9", "3.10"]
-)
+@nox.session(python=["3.8", "3.9", "3.10"])
 def tests(session):
     session.install("-r", "requirements-dev.txt")
     session.run("pytest", *session.posargs)
 
 
-@nox.session(
-    python=["3.9"]
-)
-def flake8(session):
+@nox.session(python=["3.9"])
+def ruff(session):
     session.install("-r", "requirements-dev.txt")
-    session.run("flake8", "tests", "just_bin_it")
+    session.run("ruff", "tests", "just_bin_it")
